392aed4769e290e7b717e0d86706b20869cff666
Jack Yeung <jyeung.se@gmail.com>
2018-07-11 16:44:56 -0400

    Changed a class attribute type

--- config/environment.rb
+++ config/environment.rb
@@ -6,14 +6,14 @@ ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/develo
 require_all 'app'
 
 # require 'pry'
+# #
+# flight_one = Flight.new(flight_num: 1, pilot_id: 1, traveler_id: 1)
+# # flight_two = Flight.new(name: "UA")
 #
-flight_one = Flight.new(flight_num: 1, pilot_id: 1, traveler_id: 1)
-# flight_two = Flight.new(name: "UA")
-
-traveler_one = Traveler.new(name: "Jack")
-traveler_two = Traveler.new(name: "Lily")
-
-pilot_one = Pilot.new(name: "Dick")
-pilot_two = Pilot.new(name: "Elbin")
+# traveler_one = Traveler.new(name: "Jack")
+# traveler_two = Traveler.new(name: "Lily")
 #
+# pilot_one = Pilot.new(name: "Dick")
+# pilot_two = Pilot.new(name: "Elbin")
+# #
 binding.pry
--- db/migrate/002_create_flights.rb
+++ db/migrate/002_create_flights.rb
@@ -2,7 +2,7 @@ class CreateFlights < ActiveRecord::Migration[4.2]
 
   def change
      create_table :flights do |t|
-     t.integer :flight_num
+     t.string :flight
      t.integer :traveler_id
      t.integer :pilot_id
    end
