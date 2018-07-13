99ce789dc72c71580a930f79c35c55be7d02fb9a
Jack Yeung <jyeung.se@gmail.com>
2018-07-13 02:17:37 -0400

    added notes to run file

--- bin/run.rb
+++ bin/run.rb
@@ -20,3 +20,20 @@ y_n_response = gets.chomp
 traveler_instance = Traveler.find_by(name:name)
 
 traveler_instance.yes_no_input(y_n_response)
+
+puts "May we assist you with anything else?"
+
+y_n_reponse = gets.chomp
+
+puts "Okay. Please select from the available options:"
+
+# 1. show the highest rated pilot
+# 2. show the earliest flight of the travler? (add date to flight class (ex: 07-12-2018))
+# 3. which of the traveler's flight is the shortest duration.
+# 4. Which pilot has the most years of experience flying. (add year_exp to pilot class)
+
+#
+#
+#
+
+puts "Have a great day and safe travels. Thank you for flying with Mochi Airlines"
